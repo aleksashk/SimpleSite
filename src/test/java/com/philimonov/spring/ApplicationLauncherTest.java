191b1d7f5b1fe2bec9738ccb93073e7d64f6f4a6
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-03-25 10:40:54 +0300

    Initial commit

---
+++ src/test/java/com/philimonov/spring/ApplicationLauncherTest.java
@@ -0,0 +1,20 @@
+package com.philimonov.spring;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ApplicationLauncher.
+ */
+public class ApplicationLauncherTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
